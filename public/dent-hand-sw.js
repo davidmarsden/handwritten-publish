84262ca96a643aa1a5603c8d03f6a5e8a66f66a1
davidmarsden <dmarsd@gmail.com>
2026-09-12 19:57:05 +0100

    Keep Dent Hand PWA cache current

--- public/dent-hand-sw.js
+++ public/dent-hand-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dent-hand-shell-v4';
+const CACHE = 'dent-hand-shell-v5';
 const SHELL = ['/social/', '/dent-hand.webmanifest', '/dent-hand-icon.svg'];
 
 async function precacheShell() {
