da858f92475dfeea24fa9aca5e925c4f5b5ac730
davidmarsden <dmarsd@gmail.com>
2026-09-12 21:44:31 +0100

    Refresh Dent Hand PWA shell for native content

--- public/dent-hand-sw.js
+++ public/dent-hand-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dent-hand-shell-v5';
+const CACHE = 'dent-hand-shell-v6';
 const SHELL = ['/social/', '/dent-hand.webmanifest', '/dent-hand-icon.svg'];
 
 async function precacheShell() {
