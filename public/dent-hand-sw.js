2889783fbbc5debb025bff5622269fce44794083
davidmarsden <dmarsd@gmail.com>
2026-09-12 23:45:02 +0100

    Refresh Dent Hand masthead cache

--- public/dent-hand-sw.js
+++ public/dent-hand-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dent-hand-shell-v8';
+const CACHE = 'dent-hand-shell-v9';
 const SHELL = ['/social/', '/dent-hand.webmanifest', '/dent-hand-icon.svg', '/dent-hand-brand.css', '/dent-hand-wordmark.svg'];
 
 async function precacheShell() {
