7706c8b9b90c66fb461b0ca03e6e6bafae996cf4
davidmarsden <dmarsd@gmail.com>
2026-09-12 19:56:31 +0100

    Bump Dent Hand shell after profile changes

--- public/dent-hand-sw.js
+++ public/dent-hand-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dent-hand-shell-v3';
+const CACHE = 'dent-hand-shell-v4';
 const SHELL = ['/social/', '/dent-hand.webmanifest', '/dent-hand-icon.svg'];
 
 async function precacheShell() {
