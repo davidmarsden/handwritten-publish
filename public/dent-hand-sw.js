bcc17fc01c2a2480f42664210e833208c80a60af
davidmarsden <dmarsd@gmail.com>
2026-09-13 14:05:46 +0100

    Refresh Dent Hand PWA shell for provider bootstrap

--- public/dent-hand-sw.js
+++ public/dent-hand-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dent-hand-shell-v10';
+const CACHE = 'dent-hand-shell-v11';
 const SHELL = ['/social/', '/dent-hand.webmanifest', '/dent-hand-icon.svg', '/dent-hand-brand.css', '/dent-hand-wordmark.svg'];
 
 async function precacheShell() {
